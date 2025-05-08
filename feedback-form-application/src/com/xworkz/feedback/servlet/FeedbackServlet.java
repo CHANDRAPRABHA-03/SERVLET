6a02b34151122e9a18273e53347b5ccbd4f6a8ca
CHANDRAPRABHA-03 <vchandraprabha903@gmail.com>
2025-05-08 10:08:36 +0530

    Commit

---
+++ feedback-form-application/src/com/xworkz/feedback/servlet/FeedbackServlet.java
@@ -0,0 +1,28 @@
+package com.xworkz.feedback.servlet;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+
+@WebServlet(urlPatterns = "/submit")
+public class FeedbackServlet extends GenericServlet {
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("Server is Running");
+        String name = servletRequest.getParameter("name");
+        String email = servletRequest.getParameter("email");
+        String number = servletRequest.getParameter("number");
+        String feedback = servletRequest.getParameter("feedback");
+        System.out.println("name:" +name+  "E-mail" +email+  "ContactNumber" +number+  "feedback"  +feedback);
+
+        servletResponse.setContentType("text/html");
+        PrintWriter writer = servletResponse.getWriter();
+        writer.println(name+"Thanks for submitting");
+    }
+
+}
