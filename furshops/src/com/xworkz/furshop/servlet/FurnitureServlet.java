2dd177190487e7431acdad5070b04fa1eb428340
CHANDRAPRABHA-03 <vchandraprabha903@gmail.com>
2025-05-21 23:13:20 +0530

    Commit

---
+++ furshops/src/com/xworkz/furshop/servlet/FurnitureServlet.java
@@ -0,0 +1,7 @@
+package com.xworkz.furshop.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+@WebServlet(urlPatterns = "/fur")
+public class FurnitureServlet extends HttpServlet {
+}
